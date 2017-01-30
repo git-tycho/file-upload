11e6a803a173075f934d3164596c22ea51c50f0c
Terence Bennell <tycho@mbp.local>
2017-01-30 09:39:57 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:8081/postEmail' });
+
+  ngOnInit() {
+
+    this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+      form.append('contactName', form.name);
+      form.append('contactEmail', form.email);
+      console.log(form);
+    };
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FileSelectDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
